28c1e7b4ea468638a0d19c685e21e749901a81c3
Ocunidee <ocunidee@gmail.com>
2025-02-13 22:15:12 +0100

    Demonstrate the use of FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common'
+import { Component } from '@angular/core'
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'atpw-reactive-form';
+  readonly flavors = ['vanilla', 'caramel', 'chocolate']
+  readonly iceCreamForm = new FormGroup({
+    customerName: new FormControl('Charlotte Smith'),
+    flavor: new FormControl('', Validators.required),
+    toppings: new FormGroup({
+      first: new FormControl('Whipped cream'),
+      second: new FormControl('Chocolate sauce')
+    })
+  })
+
+  submitForm() {
+    console.log('form value:', this.iceCreamForm.value)
+    console.log('is form valid?', this.iceCreamForm.valid)
+  }
 }
