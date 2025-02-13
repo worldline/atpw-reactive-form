aba14d15cb465c515f166fd3a08686f582ccb087
Ocunidee <ocunidee@gmail.com>
2025-02-13 22:21:00 +0100

    Demonstrate the use of validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common'
 import { Component, signal, inject } from '@angular/core'
-import { Validators, ReactiveFormsModule, NonNullableFormBuilder } from '@angular/forms'
+import { Validators, ReactiveFormsModule, NonNullableFormBuilder, AbstractControl } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,10 @@ import { Validators, ReactiveFormsModule, NonNullableFormBuilder } from '@angula
 export class AppComponent {
   private fb = inject(NonNullableFormBuilder)
 
-  readonly flavors = signal(['vanilla', 'caramel', 'chocolate'])
+  readonly flavors = signal(['vanilla', 'caramel', 'chocolate', 'forbidden flavor'])
   readonly iceCreamForm = this.fb.group({
-    customerName: 'Charlotte Smith',
-    flavor: ['', Validators.required],
+    customerName: ['Charlotte Smith', [Validators.required, Validators.minLength(4)]],
+    flavor: ['', [Validators.required, flavorNameValidator]],
     toppings: this.fb.group({
       first: 'Whipped cream',
       second: 'Chocolate sauce'
@@ -25,4 +25,14 @@ export class AppComponent {
     console.log('form value:', this.iceCreamForm.value)
     console.log('is form valid?', this.iceCreamForm.valid)
   }
+}
+
+
+function flavorNameValidator(control: AbstractControl): Record<string, string> | null {
+  const flavors = ['vanilla', 'caramel', 'chocolate']
+  if (flavors.includes(control.value)) {
+    return null
+  }
+
+  return { 'bad flavor': `valid flavors are ${flavors}` }
 }
